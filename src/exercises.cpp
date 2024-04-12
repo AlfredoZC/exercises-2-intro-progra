bedf89e61e1181bfd28f62d255661180caad4486
AlfredoZC <159178503+AlfredoZC@users.noreply.github.com>
2024-04-12 14:48:56 -0400

    Exercise_16 finally

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -295,7 +295,7 @@ void exercise_16(int divident, int divider) {
     int cociente = 0;
     
     
-    if(divisor != 0){
+    if(divider != 0){
         
         if(divident < divider){
             
